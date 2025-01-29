e70baf9ccbd99c4885651074620bd1aedf1e5a4e
Sudhan M <98807138+sudh4n@users.noreply.github.com>
2025-01-29 20:53:51 +0530

    1) Find the maximum element

---
+++ Random Problems/1) Find the maximum element.py
@@ -0,0 +1,13 @@
+"""Question:
+
+input = [10, 25, 5, 75, 40]
+Output: 75  
+
+Since Python uses zero-based indexing, input[3] refers to the 4th element, which is 75.
+
+Now, can you try solving the "Find the Maximum Element" problem without using max()? 😊"""
+
+Solution:
+
+input = [10, 25, 5, 75, 40]
+print(input[3])
